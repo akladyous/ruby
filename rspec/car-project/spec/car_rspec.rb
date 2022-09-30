49e2e197670d514f12835eb245f498b277ea356a
Boula Akladyous <akladyous@gmail.com>
2022-09-30 13:03:57 -0400

    updated car_rspec.rb

--- rspec/car-project/spec/car_rspec.rb
+++ rspec/car-project/spec/car_rspec.rb
@@ -4,28 +4,29 @@ describe 'Car' do
 
 
   describe 'attributes' do
+
+    before(:all) do
+      @car = Car.new
+    end
+
     it "it allows reading and writing :make" do
-      car = Car.new
-      car.make = 'Renault'
-      expect(car.make).to eq('Renault')
+      @car.make = 'Renault'
+      expect(@car.make).to eq('Renault')
     end
 
     it "it allows reading and writing :year" do
-      car = Car.new
-      car.year = 2020
-      expect(car.year).to eq(2020)
+      @car.year = 2020
+      expect(@car.year).to eq(2020)
     end
 
     it "will match numeric range" do
-      car = Car.new
-      car.year = 1995
-      expect(car.year).to be_between(1995, 2022).inclusive
+      @car.year = 1995
+      expect(@car.year).to be_between(1995, 2022).inclusive
     end
 
     it "it allows reading and writing :color" do
-      car = Car.new
-      car.color = 'Red'
-      expect(car.color).to eq('Red')
+      @car.color = 'Red'
+      expect(@car.color).to eq('Red')
     end
 
   end
