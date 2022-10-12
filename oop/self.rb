6d76f30ce20b2a6c1b25cfff856ccdbb5adb9bd6
Boula Akladyous <akladyous@gmail.com>
2022-10-12 12:38:39 -0400

    created self.rb

---
+++ oop/self.rb
@@ -0,0 +1,32 @@
+
+class One
+  class << self
+    def hello
+      puts "hello from    : #{self.class}"
+    end
+  end
+  def self.whoami
+    puts "self is       : #{self}"
+    puts "self class is : #{self.class}"
+  end
+end
+One.hello
+One.whoami
+one = One.new
+puts one.singleton_class
+
+
+class Person
+end
+p1=Person.new
+def p1.name
+  puts "ciao"
+  puts "singleton method self       : #{self}"
+  puts "singleton method self.class : #{self.class}"
+end
+p1.name
+class Class
+  def your_name
+    "#{name.upcase}"
+  end
+end
