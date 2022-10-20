4876bf6529f69cc51e2b4844abae7e20009faddf
Boula Akladyous <akladyous@gmail.com>
2022-10-20 10:59:52 -0400

    rspec

--- method/method_args.rb
+++ method/method_args.rb
@@ -2,6 +2,8 @@ def test(a=1,b=1,c=1)
 
 end
 p method(:test).arity           # 3
+                                # Returns a nonnegative integer for methods that take a fixed number of arguments
+                                #
 p method(:test).parameters      # [[:req, :a], [:req, :b], [:req, :c]]
 p method(:test).parameters.map{ |arg| arg.last }.map(&:to_s) # get method argument names !!!!
 
