460fee4a055af3e27664d649fddaf05e3913676d
Boula Akladyous <akladyous@gmail.com>
2022-10-28 12:28:26 -0400

    instance variable

--- test/lib/Ticket.rb
+++ test/lib/Ticket.rb
@@ -1,14 +1,14 @@
-# class Ticket
-#     attr_accessor :name, :title, :price
-#     @@all = []
-#     def initialize(name, title)
-#         @name = name
-#         @title = title
-#         @price = 5.00
-#         @@all << self
-#     end 
+class Ticket
+    attr_accessor :name, :title, :price
+    @@all = []
+    def initialize(name, title)
+        @name = name
+        @title = title
+        @price = 5.00
+        @@all << self
+    end
 
-#     def self.all
-#         @@all
-#     end
-# end
+    def self.all
+        @@all
+    end
+end
