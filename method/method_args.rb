736705eca57ccb14d951aa17d46b8df03d2c84ae
Boula Akladyous <akladyous@gmail.com>
2023-04-10 12:35:36 -0400

    Untracked files on ruby

--- method/method_args.rb
+++ method/method_args.rb
@@ -7,16 +7,3 @@ p method(:test).arity           # 3
 p method(:test).parameters      # [[:req, :a], [:req, :b], [:req, :c]]
 p method(:test).parameters.map{ |arg| arg.last }.map(&:to_s) # get method argument names !!!!
 
-
-def prova
-  puts "hello"
-end
-
-
-def test1
-  x = 10
-end
-def xProva
-  x = 10
-end
-
