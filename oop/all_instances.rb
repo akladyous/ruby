e9cb9f8585631852f5e3425a7cd87b75c567300f
Boula Akladyous <akladyous@gmail.com>
2023-04-26 12:06:15 -0400

    Untracked files on ruby

--- oop/all_instances.rb
+++ oop/all_instances.rb
@@ -2,28 +2,34 @@ require 'debug'
 class Person
   @@person_all = []
   attr_accessor :first_name, :last_name
-  def initialize(first_name,last_name)
+
+  def initialize(first_name, last_name)
     self.first_name = first_name
     self.last_name = last_name
     @@person_all << self
   end
+
   def self.inherited(subclass)
     subclass.define_method :full_name do
-      [self.first_name, self.last_name].join(' ')
+      [first_name, last_name].join(' ')
     end
   end
+
   def self.person_instances
     @@person_all
   end
 end
+
 class Student < Person
   @@student_all = []
   attr_accessor :course
+
   def initialize(first_name, last_name, course)
     super(first_name, last_name)
     self.course = course
     @@student_all << self
   end
+
   def self.student_instances
     @@student_all
   end
