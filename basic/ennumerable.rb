13645c1bcf902670897a222541b7bddec5602de4
Boula Akladyous <akladyous@gmail.com>
2023-04-26 15:43:48 -0400

    Untracked files on ruby

--- basic/ennumerable.rb
+++ basic/ennumerable.rb
@@ -11,3 +11,6 @@ Array.new(4) { rand > 0.5 }
 [1, 2, 3, 4, 5].one? { |num| num > 3 }
 
 Array.new(10) { rand(10) }.count { |num| num > 3 }
+{ a: 1, b: 2, c: 3, d: 4 }.count { |k, v| v > 2 }
+
+Array.new(10) { rand(97..122) }.map { |num| num.chr }.tally # {"a"=>1, "g"=>2, "y"=>2, "u"=>2, "m"=>1, "t"=>1, "q"=>1}
