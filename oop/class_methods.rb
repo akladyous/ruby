469cd18d24141e73641d9c48307d281ffdeac4fe
Boula Akladyous <akladyous@gmail.com>
2023-04-27 15:24:34 -0400

    Untracked files on ruby

--- oop/class_methods.rb
+++ oop/class_methods.rb
@@ -2,16 +2,20 @@ class User
   attr_accessor :first_name, :last_name, :age
 
   def initialize(first_name, last_name, age)
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+    @first_name = first_name
+    @last_name  = last_name
+    @age        = age
+
   end
 
   def full_name
-    "#{self.first_name.capitalize} #{self.last_name.capitalize}"
+    "#{@first_name.capitalize} #{self.last_name.capitalize}"
   end
 
 end
 
 john = User.new "john", "doe", 21
 print(john.full_name)
+
+
+
