0a7a8cbddd8a8738a7d58f75cc4bd5bce6d895ef
Boula Akladyous <akladyous@gmail.com>
2023-04-27 14:45:20 -0400

    Untracked files on ruby

--- basic/ennumerable.rb
+++ basic/ennumerable.rb
@@ -18,6 +18,7 @@ a1.count { |num| num > 3 }      # 8
 d1 = { a: 1, b: 2, c: 3, d: 4 }
 d1.count { |k, v| v > 2 }   # 2
 
+[*(1..10)].each { |num| break if num > 5; p num }
 
 Array.new(10) { rand(97..122) }.map { |num| num.chr }.tally # {"a"=>1, "g"=>2, "y"=>2, "u"=>2, "m"=>1, "t"=>1, "q"=>1}
 
