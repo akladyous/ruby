98dfa5096a4b5c5139f6f89ca606d7d8eec3ed92
Boula Akladyous <akladyous@gmail.com>
2023-04-27 15:14:26 -0400

    Untracked files on ruby

---
+++ oop/class_methods.rb
@@ -0,0 +1,15 @@
+class User
+  attr_accessor :first_name, :last_name, :age
+
+  def initialize(first_name, last_name, age)
+    self.first_name = first_name
+    self.last_name = last_name
+    self.age = age
+  end
+
+  def full_name
+    "#{self.first_name}. #{self.last_name}"
+  end
+
+end
+
