9a16398dc5cc62eabb8318c64823ab330bb16179
Boula Akladyous <akladyous@gmail.com>
2023-04-27 15:39:17 -0400

    Untracked files on ruby

--- oop/class_methods.rb
+++ oop/class_methods.rb
@@ -11,11 +11,28 @@ class User
   def full_name
     "#{@first_name.capitalize} #{self.last_name.capitalize}"
   end
+  def get_password
+    @password
+  end
+  def set_password(new_password)
+    @password = new_password
+  end
+
+  private
+    @password
 
 end
 
 john = User.new "john", "doe", 21
 print(john.full_name)
-
-
-
+p john.get_password
+p "-" * 50
+puts "public methods"
+p "-" * 50
+puts User.public_methods false
+puts "private methods"
+p "-" * 50
+puts User.private_methods false
+puts "protected methods"
+p "-" * 50
+puts User.protected_methods false
