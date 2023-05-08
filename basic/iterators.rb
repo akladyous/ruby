ac76855f3047d688e7527b2fbd49d6f7cfb15826
Boula Akladyous <akladyous@gmail.com>
2023-05-08 11:34:46 -0400

    Untracked files on ruby

---
+++ basic/iterators.rb
@@ -0,0 +1,13 @@
+require 'debug'
+
+
+def sum_numbers(*nums)
+  total = 0
+  nums.each_with_index do |num, idx|
+    total += num
+    yield total if block_given?
+  end
+  total
+end
+
+sum_numbers(1,1,1) { |x| puts x}
