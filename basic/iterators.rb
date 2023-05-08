4fc1c075020fc88e1e091c5a85d16968ab801748
Boula Akladyous <akladyous@gmail.com>
2023-05-08 14:00:48 -0400

    Untracked files on ruby

--- basic/iterators.rb
+++ basic/iterators.rb
@@ -11,3 +11,18 @@ def sum_numbers(*nums)
 end
 
 sum_numbers(1,1,1) { |x| puts x}
+
+
+fib = Enumerator.new do |y|
+  a = b = 1
+  loop do
+    debugger
+    y << a
+    a, b = b, a + b
+  end
+end
+fib.take(10)
+
+
+enum = %W[one two three four]
+enum.each_with_object([]) { |item, obj| puts obj[0]; obj.push('test') }
